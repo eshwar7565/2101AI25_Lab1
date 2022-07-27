af2fa0979909c0e8f1d001181103d4b902a4a56a
eshwar7565 <110085174+eshwar7565@users.noreply.github.com>
2022-07-27 14:41:35 +0530

    selection sort

--- 2.c
+++ 2.c
@@ -16,3 +16,21 @@ void printArray(int* A, int n)
     }
     printf("\n");
 }
+void selectionSort(int *A, int n){
+    int indexOfMin, temp;
+    printf("Running Selection sort...\n");
+    for (int i = 0; i < n-1; i++)
+    {
+        indexOfMin = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if(A[j] < A[indexOfMin]){
+                indexOfMin = j;
+            }
+        }
+        // Swap A[i] and A[indexOfMin]
+        temp = A[i];
+        A[i] = A[indexOfMin];
+        A[indexOfMin] = temp;
+    }
+}
