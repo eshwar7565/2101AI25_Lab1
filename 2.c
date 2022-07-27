b3bd3a908bb04c183a326a30140532edf798b353
eshwar7565 <110085174+eshwar7565@users.noreply.github.com>
2022-07-27 14:40:09 +0530

    insertion sort

--- 2.c
+++ 2.c
@@ -7,3 +7,12 @@ void printArray(int* A, int n)
     }
     printf("\n");
 }
+#include<stdio.h>
+void printArray(int* A, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
